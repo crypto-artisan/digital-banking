6bb343efe4bfcac4a9654c418102674786007066
hightecular95908 <zulhelmijohn0218@gmail.com>
2022-02-28 15:42:30 +0800

    fix: intro part style

--- src/pages/landing/IntroPart.tsx
+++ src/pages/landing/IntroPart.tsx
@@ -54,7 +54,7 @@ const IntroPart = () => {
         </div>
       </div>
       <Container>
-        <div className="grid grid-cols-1 lg:grid-cols-2 text-white pb-16 pt-10">
+        <div className="grid grid-cols-1 lg:grid-cols-2 items-center text-white pb-16 pt-10">
           <div>
             <h1 className="text-5xl sm:text-6xl md:text-7xl leading-tight">
               Get your government benefits up to&nbsp;
@@ -71,12 +71,12 @@ const IntroPart = () => {
               </div>
             </div>
           </div>
-          <div className="hidden lg:flex relative items-end">
+          <div className="hidden lg:flex items-end">
             <img
               src={imacPhone}
               alt="imacPhone"
               style={{ filter: "brightness(105%) saturate(120%)" }}
-              className="w-full absolute bottom-0"
+              className="w-full"
             />
           </div>
         </div>
